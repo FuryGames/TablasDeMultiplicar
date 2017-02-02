c4b6ec24943fafba2974739ddf8c94c5b6b4018c
Diego Valencia <diegovalencia@MacBook-Air-de-Diego.local>
2017-02-01 22:42:58 -0300

    Arreglo de scripts
    Arreglo de scripts y pruebas

--- src/Singletons/Mainpanellevels.gd
+++ src/Singletons/Mainpanellevels.gd
@@ -4,7 +4,7 @@ extends Panel
 # var a = 2
 # var b = "textvar"
 const CNIVELES = 5 #cantidad de niveles 
-var niveles=[1,1,1,0,0]#1 para nivel desbloqueado, 0 para nivel bloqueado
+var niveles=[1,0,0,0,0]#1 para nivel desbloqueado, 0 para nivel bloqueado
 
 func _ready():
 	Check_completed_lvls()
@@ -14,24 +14,22 @@ func _ready():
 #Reinicia los niveles, como si empezara todo denuevo
 func Reset_lvls():
 	var i=0
-	for i in range(CNIVELES):
+	for i in range(1,CNIVELES):
 		niveles[i] = 0
 	Check_completed_lvls()
 
-#ve que niveles ha pasado el usuario
+#ve que niveles ha pasado el usuario,ademas sirve como refresco de pantalla para actualizazr niveles avanzados
 func Check_completed_lvls():
 	var i = 0
 	for i in range(CNIVELES):
+		var aux = "TextureButton"
+		var nombre_nodo = aux + str(i+1)
 		if(niveles[i] == 0):
-			var aux = "TextureButton"
-			var nombre_nodo = aux + str(i+1)
-			get_node(nombre_nodo).Disable()
-func _on_TextureButton_pressed():
-	pass
+			get_node(nombre_nodo).set_disabled(true)
+		get_node(nombre_nodo).get_node("Label").set_text(str(i))
 
 
 func _on_TextureButton1_pressed():
-	pass
-
+		get_tree().change_scene("res://src/Levels/Level1.tscn")
 func _on_boton_reiniciar_pressed():
 	Reset_lvls() # replace with function body
